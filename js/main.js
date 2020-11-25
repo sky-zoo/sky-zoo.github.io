472d2eb13f39d910567cc52b05456e4a158230d9
Rodri <rodrigo-1905@hotmail.com>
2020-11-25 13:35:12 -0300

    Agrego todos los estilos responsive y creé el menu, falta agregar y modificar el contenido de sobre-mi y los proyectos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 document.getElementById("botonMenu").addEventListener("click", function(e){
-    let linksNavegacion = document.getElementById("menu");
+    let linksNavegacion = document.getElementById("linksNavegacion");
     // let cerrarMenu = document.getElementById("cerrarMenu");
     linksNavegacion.classList.toggle("visible");
     
